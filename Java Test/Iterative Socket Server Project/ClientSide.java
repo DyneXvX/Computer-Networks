915ed6f9840e8695cfaae0b61f64f27323633431
Justn Thoms <DyneXvX@outlook.com>
2021-01-20 16:21:44 -0500

    Started Client and reading instructions.

---
+++ Java Test/Iterative Socket Server Project/ClientSide.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class ClientSide {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to Iterative socket server project");
+        System.out.println("Brought to you by Justin Thoms");
+        System.out.println("..............................");
+
+        //System input information:
+        System.out.println("Please input the network address you wish to test today.");
+
+        while(true) {
+
+        }
+    }
+}
