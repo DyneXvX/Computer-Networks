dbcec4e0efa04bee589d0f6f2efecade56039bbb
Justn Thoms <DyneXvX@outlook.com>
2021-01-23 16:47:43 -0500

    Going through the directions and online guilde.

--- Java Test/Iterative Socket Server Project/ClientSide.java
+++ Java Test/Iterative Socket Server Project/ClientSide.java
@@ -1,17 +1,37 @@
 import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class ClientSide {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to Iterative socket server project");
         System.out.println("Brought to you by Justin Thoms");
         System.out.println("..............................");
 
-        //System input information:
-        System.out.println("Please input the network address you wish to test today.");
+        try {
 
-        while(true) {
+            // System input information:
+            // User network address requested:
+            System.out.println("Please input the network address you wish to test today.");
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            String address = input.readLine();
 
+            // User Port address requested:
+            System.out.println("Please input the port to use.");
+            int port = Integer.parseInt(input.readLine());
+            
+            //note these can through IO Exceptions build a catch for this.
+            Socket socket = new Socket(InetAddress.getByName(address), port);
+
+            //From class zoom menu:
+            //And directly taken option from instructions.
+            System.out.println("How many client request do you want to generate?");
+            int request = Integer.parseInt(input.readLine());
+
+        } 
+        catch (IOException exception) {
+            exception.printStackTrace();
         }
     }
 }
---
+++ Java Test/Iterative Socket Server Project/ServerSide.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class ServerSide {
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println("---Starting up the Server Side----");
+        System.out.println("Waiting for the Client to connect.");
+        System.out.println("----------------------------------");
+
+        //Once again possible IO Exceptions build catch.
+        ServerSocket serverSocket = new ServerSocket(6868);
+        Socket socket = serverSocket.accept();
+        System.out.println("The connection has been accepted.");
+
+        //Get information from the the ClientSide
+        InputStream input = socket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+
+        // Date and Time - the date and time on the server
+        // Uptime - how long the server has been running since last boot-up
+        // Memory Use - the current memory usage on the server
+        // Netstat - lists network connections on the server
+        // Current Users - list of users currently connected to the server
+        // Running Processes - list of programs currently running on the server
+        
+        //Build output to the ClientSide
+        OutputStream output = socket.getOutputStream();
+        PrintWriter writer = new PrintWriter(output, true);
+
+        //Build Client Choices for testing
+        while (true){
+            String choice = reader.readLine();
+            switch(choice){
+                case "1":
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+                    LocalDateTime timeNow = LocalDateTime.now();
+                    writer.println(formatter.format(timeNow));
+                    continue;
+                
+                case "Exit":
+                    writer.println("ServerSide shutting down.");
+                    input.close();
+                    writer.close();
+                    break;
+                default:
+                    writer.println("Justin we broke something.");
+
+            }
+            break;
+        }
+        serverSocket.close();
+    }
+}
