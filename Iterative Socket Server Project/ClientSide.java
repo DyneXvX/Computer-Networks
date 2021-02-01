3130ffb9a0bad974558ce2f4ebdab85e409c7da8
Justn Thoms <DyneXvX@outlook.com>
2021-02-01 16:17:48 -0500

    Building Scripts

--- Iterative Socket Server Project/ClientSide.java
+++ Iterative Socket Server Project/ClientSide.java
@@ -1,10 +1,13 @@
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.time.Duration;
+import java.time.Instant;
 
 public class ClientSide {
 
     public static void main(String[] args) {
+        System.out.println();
         System.out.println("Welcome to Iterative socket server project");
         System.out.println("Brought to you by Justin Thoms");
         System.out.println("..............................");
@@ -19,6 +22,7 @@ public class ClientSide {
 
             // User Port address requested:
             System.out.println("Please input the port to use.");
+            input = new BufferedReader(new InputStreamReader(System.in));
             int port = Integer.parseInt(input.readLine());
             
             //note these can through IO Exceptions build a catch for this.
@@ -27,8 +31,65 @@ public class ClientSide {
             //From class zoom menu:
             //And directly taken option from instructions.
             System.out.println("How many client request do you want to generate?");
+            input = new BufferedReader(new InputStreamReader(System.in));
             int request = Integer.parseInt(input.readLine());
 
+            //send information back and forth between Server and Client
+            PrintWriter out;
+            BufferedReader in;
+
+            //setup the timers
+            Instant start;
+            Instant end;
+            Duration timeRan;
+            Duration total = Duration.ZERO;
+
+            //Menu
+            System.out.println("-----------------Menu-------------------------------------");
+            System.out.println("------------Please enter a number-------------------------");
+            System.out.println("1 - Date and Time on the Server");
+            System.out.println("2 - For the Server Uptime");
+            System.out.println("3 - For Current Memory Use on the Server");
+            System.out.println("4 - For Net Stats ('List of Current Connections on the Server ')");
+            System.out.println("5 - For a list of Current Users on the Server");
+            System.out.println("6 - For a list of Current Running Processes on the Server");
+            System.out.println("---------------------------------------------------------");
+            
+
+            
+            for (int i = 0; i < request; i ++)
+            {
+                System.out.println("Make a request please");
+
+                //get menu choice from the user
+                BufferedReader menuChoice = new BufferedReader(new InputStreamReader(System.in));
+                menuChoice = new BufferedReader(new InputStreamReader(System.in));
+                int choice = Integer.parseInt(menuChoice.readLine());
+
+                out = new PrintWriter(socket.getOutputStream(), true);
+                start = Instant.now();
+                out.println(choice);
+
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                end = Instant.now();
+                
+                timeRan = Duration.between(start, end);
+                total = total.plus(timeRan);
+
+                //display time to the client
+                System.out.println(in.readLine());
+                System.out.println("The time it took was: " + timeRan.toNanos() + " nanoseconds.");
+                
+                //close connection if total will be what the requested number is.
+                if((i + 1) == request)
+                {
+                    out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println("End");
+                    in.close();
+                    out.close();
+                }
+
+            }
         } 
         catch (IOException exception) {
             exception.printStackTrace();
--- Iterative Socket Server Project/ServerSide.java
+++ Iterative Socket Server Project/ServerSide.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,49 +11,55 @@ import java.time.format.DateTimeFormatter;
 
 public class ServerSide {
     public static void main(String[] args) throws IOException {
+        System.out.println();
         System.out.println("---Starting up the Server Side----");
         System.out.println("Waiting for the Client to connect.");
         System.out.println("----------------------------------");
+        System.out.println("Host: 139.62.210.153");
+        System.out.println("Justin the port is 3287");
 
         // Once again possible IO Exceptions build catch.
-        ServerSocket serverSocket = new ServerSocket(6868);
+        ServerSocket serverSocket = new ServerSocket(3287);
         Socket socket = serverSocket.accept();
-        System.out.println("The connection has been accepted.");
 
-        // Get information from the the ClientSide
+        System.out.println("The connection has been accepted.");       
+        BufferedReader in;
+
+        // Get information from the ClientSide and send it back out
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         InputStream input = socket.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
+        
+        //---Requested Build Methods for Class---
         // Date and Time - the date and time on the server
         // Uptime - how long the server has been running since last boot-up
         // Memory Use - the current memory usage on the server
-        // Netstat - lists network connections on the server
+        // Net Stat - lists network connections on the server
         // Current Users - list of users currently connected to the server
         // Running Processes - list of programs currently running on the server
 
-        // Build output to the ClientSide
-        OutputStream output = socket.getOutputStream();
-        PrintWriter writer = new PrintWriter(output, true);
-
         // Build Client Choices for testing
         while (true) {
-            String choice = reader.readLine();
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String choice = in.readLine();
             switch (choice) {
                 case "1":
                     LocalDate date = LocalDate.now();
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
                     LocalDateTime timeNow = LocalDateTime.now();
-                    writer.println(date);
-                    writer.println(formatter.format(timeNow));
+                    out.println(date);
+                    out.println(formatter.format(timeNow));
                     continue;
 
-                case "Exit":
-                    writer.println("ServerSide shutting down.");
+                case "End":
+                    out.println("ServerSide shutting down.");
                     input.close();
-                    writer.close();
+                    out.close();
                     break;
                 default:
-                    writer.println("Justin we broke something.");
+                    out = new PrintWriter(socket.getOutputStream(), true);    
+                    out.println("Justin we broke something.");
+                    out.flush();
+                    
 
             }
             break;
